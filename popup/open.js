10afa3e08eada166f64ef1d9d0c75730053614ca
kevinangkajaya <kevinangkajaya.gukguk@gmail.com>
2025-12-29 20:15:19 +0700

    findGalleryCarousel from popup

--- popup/open.js
+++ popup/open.js
@@ -1,20 +1,86 @@
+const getActiveTab = async () => {
+    let tabs = await browser.tabs.query({ active: true, currentWindow: true })
+
+    var currTab = tabs[0];
+    if (currTab) { // Sanity check
+        return currTab
+    } else {
+        throw new Error("No currTab")
+    }
+}
+
+const testing = () => {
+
+    getActiveTab().then((activeTab) => {
+
+        browser.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            func: () => {
+                document.body.style.border = "5px solid blue";
+                console.log(document.body.style.border)
+            }
+        })
+    })
+}
+
+const findGalleryCarousel = async () => {
+    let activeTab = await getActiveTab()
+
+    const imgSrc = await browser.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        func: () => {
+            // ============================ find gallery carousel ============================
+            const galleryCarousels = document.getElementsByTagName('gallery-carousel')
+
+            // ============================ find images on reddit ============================
+            const imgSrc = new Set();
+            for (const galleryCarousel of galleryCarousels) {
+
+                const lis = galleryCarousel.getElementsByTagName('li')
+
+                for (const li of lis) {
+                    const imgs = li.getElementsByTagName('img')
+
+                    for (const img of imgs) {
+                        if (img.src) {
+                            imgSrc.add(img.src)
+                        } else if (img.getAttribute("data-src")) {
+                            imgSrc.add(img.getAttribute("data-src"))
+                        } else if (img.getAttribute("data-lazy-src")) {
+                            imgSrc.add(img.getAttribute("data-lazy-src"))
+                        }
+                    }
+                }
+            }
+
+            return imgSrc;
+
+        }
+    })
+
+    return imgSrc;
+}
+
+
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
+const listenForClicks = () => {
+
+    document.getElementById("open-popup").addEventListener("click", (e) => {
         console.log(e)
 
-        /**
-         * Get the active tab,
-         * then call "beastify()" or "reset()" as appropriate.
-         */
-        if (e.target.id === "open-popup") {
-            console.log("here open-popup")
-            return;
-        }
+        findGalleryCarousel().then((imgSrc) => {
+
+            console.log(imgSrc)
+            // browser.tabs.create({
+            //     url: "/image_content/image_content.html"
+            // })
+
+        })
     });
+
 }
 
 /**
@@ -22,4 +88,5 @@ function listenForClicks() {
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-browser.tabs.then(listenForClicks)
+listenForClicks()
+// testing()
